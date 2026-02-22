b7c30d18528d976e8bdac552e4c15b7fb86dddc4
ArnauSerrano <arnau.serranosoler@escolaindustrial.org>
2026-02-22 12:43:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,21 +1,10 @@
 module.exports = {
 
-    // Port (Koyeb el defineix)
     uiPort: process.env.PORT || 1880,
 
-    // IMPORTANT: secret fix via variable d'entorn
     credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
 
-    // Exposem variables al runtime
-    functionGlobalContext: {
-        GMAIL_USERID: process.env.GMAIL_USERID,
-        GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-        INFLUX_TOKEN: process.env.INFLUX_TOKEN
-    },
-
-    // Directori de treball
     userDir: '/data',
 
-    // Fitxer de flows
     flowFile: 'flows.json'
 }

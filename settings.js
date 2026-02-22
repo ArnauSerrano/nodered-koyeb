f69b9d65ae1327d444a7d97076245dec59b30b2f
ArnauSerrano <arnau.serranosoler@escolaindustrial.org>
2026-02-22 13:03:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 module.exports = {
-
     uiPort: process.env.PORT || 1880,
-
-    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
-
     userDir: '/data',
-
-    flowFile: 'flows.json'
+    flowFile: 'flows.json',
+    functionGlobalContext: {
+        GMAIL_USERID: process.env.GMAIL_USERID,
+        GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
+        INFLUX_TOKEN: process.env.INFLUX_TOKEN
+    },
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET
 }
